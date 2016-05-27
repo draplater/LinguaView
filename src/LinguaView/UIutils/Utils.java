d313ef82fa3d2aa6312aaf8d9ddda23ddf225377
draplater <tsjzadmin@gmail.com>
2016-05-27 12:27:46 +0800

    Utils: logger support.

--- src/LinguaView/UIutils/Utils.java
+++ src/LinguaView/UIutils/Utils.java
@@ -5,6 +5,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Some useful UI functions.
@@ -17,6 +19,17 @@ public class Utils {
      * @param savemode
      * @return
      */
+    public static Logger logger = Logger.getLogger("linguaview");
+    public static final boolean isDebug =
+            java.lang.management.ManagementFactory.getRuntimeMXBean().
+            getInputArguments().toString().indexOf("-agentlib:jdwp") > 0;
+
+    static {
+        if(isDebug)
+            logger.setLevel(Level.INFO);
+        else
+            logger.setLevel(Level.WARNING);
+    }
     public static String fileSelection(boolean savemode) {
         String os = System.getProperty("os.name");
         File input = null;

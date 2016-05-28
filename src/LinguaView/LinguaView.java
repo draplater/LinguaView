4e39369516ddc815801acf12aa8883685a30be13
draplater <tsjzadmin@gmail.com>
2016-05-28 09:21:28 +0800

    LinguaView: bump version to 1.2.0.

--- src/LinguaView/LinguaView.java
+++ src/LinguaView/LinguaView.java
@@ -159,7 +159,7 @@ class TabbedPaneFrame extends JFrame {
 
 	public TabbedPaneFrame() {
 		// deal with the panel: size, font, layout, etc.
-		setTitle("LinguaView 1.1.0");
+		setTitle("LinguaView 1.2.0");
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = kit.getScreenSize();
 		int screenHeight = screenSize.height;
